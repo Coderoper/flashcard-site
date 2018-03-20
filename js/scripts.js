3d83dd551e305672110c2194d662db62bfe60439
Austin Roberts and Justin Roller <austin@>
2018-03-20 14:14:54 -0700

    add jscript add and remove classes for clickable definition and term, also more styles and text for definitions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $(".javascript-hidden").slideToggle();
     $(".javascript-showing").slideToggle();
+    $(".operators-hidden").addClass("initHidden");
   });
-  $(".clickable").click(function() {
+  $(".clickable2").click(function() {
+    $(".javascript-hidden").slideToggle();
+    $(".javascript-showing").slideToggle();
+    $(".operators-hidden").removeClass("initHidden");
+  });
+  $(".clickable3").click(function() {
     $(".operators-hidden").slideToggle();
     $(".operators-showing").slideToggle();
+    $(".javascript-hidden").addClass("initHidden");
+  });
+  $(".clickable4").click(function() {
+    $(".operators-hidden").slideToggle();
+    $(".operators-showing").slideToggle();
+    $(".javascript-hidden").removeClass("initHidden");
   });
 });
