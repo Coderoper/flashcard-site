8c743cdf8266b3c9ba3538daa0f5400770091a8a
Austin Roberts and Justin Roller <austin@>
2018-03-20 11:30:43 -0700

    add body of website for definitions, jquery clickables, and definitions.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".javascript-hidden").slideToggle();
+    $(".javascript-showing").slideToggle();
+  });
+  $(".clickable").click(function() {
+    $(".operators-hidden").slideToggle();
+    $(".operators-showing").slideToggle();
+  });
+});
