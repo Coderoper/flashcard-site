06920da263c8f885a5687a0f959db73796483117
Austin Roberts and Justin Roller <justin@>
2018-03-20 15:21:44 -0700

    add more terms, cleaned up jquery script for 1 line function with parents to apply to all terms and definition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,6 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".javascript-hidden").slideToggle();
-    $(".javascript-showing").slideToggle();
-    $(".operators-hidden").addClass("initHidden");
-  });
-  $(".clickable2").click(function() {
-    $(".javascript-hidden").slideToggle();
-    $(".javascript-showing").slideToggle();
-    $(".operators-hidden").removeClass("initHidden");
-  });
-  $(".clickable3").click(function() {
-    $(".operators-hidden").slideToggle();
-    $(".operators-showing").slideToggle();
-    $(".javascript-hidden").addClass("initHidden");
-  });
-  $(".clickable4").click(function() {
-    $(".operators-hidden").slideToggle();
-    $(".operators-showing").slideToggle();
-    $(".javascript-hidden").removeClass("initHidden");
+  $(".clickable").click(function(){
+    console.log(this);
+    $(this).parent().parent().parent().find(".definition").slideToggle();
   });
 });
